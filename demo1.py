90338db678f17d570dde9a1976a07b9aedddc532
ArunAkil <arunkumar02136@gmail.com>
2021-12-14 14:44:28 +0530

    demo1.py file is added to commit

--- demo1.py
+++ demo1.py
@@ -1,1 +1,4 @@
-print("hello world")
+
+[A[A[B[B[A[A[C[[3~[3~[3~[3~[C[[C[C[3~[3~[3~[3~[C[C[C[C[C[C[C[C[git remote add origin https://github.com/ArunAkil/demo1.git
+clear
+exit
